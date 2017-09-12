7c786bf8a45a84ec8fcce7bf5374715cf6adbd5c
GupCa <anntytova@gmail.com>
2017-09-12 19:41:53 +0300

    c# HW 1

---
+++ CSharpHW/CSharpHW-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpHW_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Ann Tytova");
+            Console.ReadKey();
+        }
+    }
+}
